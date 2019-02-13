ec68422c9d99aed377136e1e82f04c0922695f18
Reactongraph <js.developer@ongraph.com>
2019-02-13 13:18:14 +0530

    Define get and set local data

--- src/utils/helper.tsx
+++ src/utils/helper.tsx
@@ -1,8 +1,9 @@
+import { AsyncStorage } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 
 export default class Helper {
   // resetNavigation to zero index
-  resetNavigation(pointer: any, navigation: any, parameter = null) {
+  resetNavigation = (pointer: any, navigation: any, parameter = null) => {
     const _this = pointer;
     _this.props.navigation.dispatch(
       NavigationActions.reset({
@@ -16,4 +17,36 @@ export default class Helper {
       })
     );
   }
+
+  // Store value in local storage
+  storeData = async (key: string, data: string) => {
+    try {
+      await AsyncStorage.setItem(key, data);
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+
+  // get value from local storage
+  getLocalData = async (key: string) => {
+    try {
+      const value = await AsyncStorage.getItem(key);
+      if (value) {
+        return JSON.parse(value)
+      }
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+
+  // email validation
+  validateData = (email: string, password: string) => {
+    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+      if (reg.test(email) === true && password.length > 5) {
+      return true;
+    }
+    alert('Invalid email or password length is greater than 5');
+    return false;
+  }
+
 }
